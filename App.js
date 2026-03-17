232dbfb394b612223b2daf71b0bfb926e534236f
rafagit1703 <rafa.dev0506@gmail.com>
2026-03-17 20:08:26 -0300

    feat: adiciona contador simples com incremento e decrement o

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+
+  const diminuir = () => {
+    setContador(contador - 1);
+  };
+  const aumentar = () => {
+    setContador(contador + 1);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require("./assets/logounipar.png")}
+        style={styles.minhaImagem}
+      />
+
+      <View style={{flexDirection: "row"}}>
+        <Text style={{marginRight: 150, fontSize: 40}}>Nós</Text>
+        <Text style={{fontSize: 40}}>Eles</Text>
+      </View>
+
+      <Text style={{ marginTop: 30, marginBottom: 20, fontSize: 35 }}>
+        {contador}
+      </Text>
+
+      <View style={styles.botoes}>
+        <TouchableOpacity style={styles.botaoAumentar} onPress={aumentar}>
+          <Text style={styles.textoBotao}>+</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.botaoDiminuir} onPress={diminuir}>
+          <Text style={styles.textoBotao}>-</Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
-      
-    </View>
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  textoNome: {
+    fontSize: 35,
+    marginRight: 10, // espaço entre os textos
+  },
+  minhaImagem: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain", //pra não cortar a imagem
+  },
+  botoes: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+
+  botaoAumentar: {
+    backgroundColor: "green",
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+  },
+
+  botaoDiminuir: {
+    backgroundColor: "red",
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  textoBotao: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
